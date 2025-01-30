a21f58d5d89f4b5854bbc2c7fb8ef5409b9e47ac
Monish.Tyagi <monishtyagi5770@gmail.com>
2025-01-30 13:38:10 +0530

    before constructor injection

---
+++ Practice/3.Spring/demo/demo/src/main/java/com/example/demo/couple/Dog.java
@@ -0,0 +1,11 @@
+package com.example.demo.couple;
+
+import org.springframework.stereotype.Component;
+
+@Component("dog")
+public class Dog  implements Animal{
+    @Override
+    public void play(){
+        System.out.println("dog is playing");
+    }
+}
